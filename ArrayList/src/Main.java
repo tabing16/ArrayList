c137806e64039dfe13f96fc62870f16b6bbda2cc
Rudy Rusli <rrusli@hotmail.com>
2022-08-16 23:41:25 +0800

    Initial commit

---
+++ ArrayList/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
+
+    public static void main(String[] args) {
+
+    }
+}
